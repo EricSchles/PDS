7b02b87f0536f831004d51a5d50b8193de8b6b0d
Eric Schles <ericschles@gmail.com>
2013-06-12 08:52:42 -0400

    half way through chapter 1

---
+++ chapter1/ex1.py
@@ -0,0 +1,30 @@
+#Extracts a collection of birth dates from the user and determines
+# if each individual is at least 21 years of age.
+from datetime import date as Date
+
+def main():
+    # Date before which a person must have been born to be 21 or older.
+    bornBefore = Date(1988,6,1) #year, month, day
+    #as of writing of the book...not true now.
+
+    #Extract birth dates from the user and determine if 21 or older.
+    date = promptAndExtractDate()
+    while date is not None:
+        if date <= bornBefore:
+            print( "Is at least 21 years of age: ", date)
+        date = promptAndExtractDate()
+
+#Prompts for and extracts the Gregorian date components.  Returns a
+# Date object or None when the user has finished entering dates.
+def promptAndExtractDate():
+    print( "Enter a birth date." )
+    month = int( input("month (0 to quit): " ) )
+    if month == 0:
+        return None
+    else:
+        day = int( input("day: ") )
+        year = int( input("year: ") )
+        return Date( year, month , day)
+
+#Call the main routine.
+main()
